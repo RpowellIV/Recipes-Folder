48c4411175d23cb341daea8ff015e022b7ff4ded
Robert Powell <r.powelliv@gmail.com>
2020-07-09 19:38:54 -0400

    Updates

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-function marinaraSauce (cookName){
-    var two = 2;
-    var oneFourth = "1/4"; 
-    var greet = "Hey, "+ cookName + "! This is my marinara sauce recipe!";
-    console.log(greet);
-    console.log("- Heat a medium-large saucepan over medium heat");
-    console.log("- Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about "+ two + " minutes");
-    console.log("- Add " + oneFourth + " cup water, "+ two +" cans of crushed tomates, and salt and season with black pepper to taste.");
-    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes");
-    console.log("- Stir in "+ oneFourth +" cup roughly chopped fresh basil, salt and pepper as needed.");
+function marinaraSauce (cookName, var2){
+    var two = 2
+    var oneFourth = "1/4"
+    var greet = "Hey, "+ cookName + "! This is my marinara sauce recipe! " + var2
+    console.log(greet)
+    console.log("- Heat a medium-large saucepan over medium heat")
+    console.log("- Add " + two + " tspns of oil and 5 garlic cloves, cook until golden, about "+ two + " minutes")
+    console.log("- Add " + oneFourth + " cup water, "+ two +" cans of crushed tomates, and salt and season with black pepper to taste.")
+    console.log("- Cover the pot, bring to a boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes")
+    console.log("- Stir in "+ oneFourth +" cup roughly chopped fresh basil, salt and pepper as needed.")
 };
 
-marinaraSauce("Adam");
+marinaraSauce("Adam","CHECK IT OUT!")
